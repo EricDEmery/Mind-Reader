aa7986c00e8c62e20a4f2631f4250b1f59ddb32b
Eric Emery <ericemery099@gmail.com>
2023-06-10 18:32:05 -0400

    updated JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Setting Initial Variables
 const page = document.getElementById("page");
 const instruct = document.getElementById("instruct");
 const nextButton = document.getElementById("next");
@@ -7,7 +8,7 @@ const example = document.getElementById("example");
 let pageNum = 0;
 let arraySym = ['!', '@', '#', '$', '%'];
 
-//Main Array For Mind Reader to Pull From
+//Main Array For Mind Reader to Pull Information From and insert into page
 let mainArr = [
     {   //Page 1
         instruct: "I can read your mind",
@@ -48,13 +49,13 @@ let mainArr = [
 
 function randomize() {
     let num = '';  //empty for output
-    sym = arraySym[Math.floor(Math.random() * 6)]; // Will randomize every 6th symbol
+    sym = arraySym[Math.floor(Math.random() * 9)]; // Will change every 9 intervals
 
-    for (let i = 0; i < 100; i++) {
-        if (i % 6 === 0) {
-            num += i + ': ' + sym + '<br>';
+    for (let i = 0; i < 100; i++) { //Will add 1 for every loop until 100
+        if (i % 9 === 0) {
+            num += i + ': ' + sym + '<br>'; //Will allow a symbol from arr to display to every multiple of 9
         } else {
-        let sym = arraySym[Math.floor(Math.random() * 6)];
+            let sym = arraySym[Math.floor(Math.random() * 9)];
             num += i + ' :' + sym + '<br>';
         }
     }
@@ -62,7 +63,7 @@ function randomize() {
 }
 
 function changePage() {
-    if (pageNum === 4) {
+    if (pageNum === 4) { //Will call randomize function on page 4
         randomize()
 
     } else if (pageNum === 5) {
@@ -75,6 +76,7 @@ function changePage() {
     instruct.innerHTML = mainArr[pageNum].instruct
     example.innerHTML = mainArr[pageNum].example
 
+    // Will change the visibility of buttons and text between pages
     if (pageNum === 0) {
         goButton.style.visibility = 'visible';
         prevButton.style.visibility = 'hidden'
@@ -93,20 +95,19 @@ function changePage() {
         nextButton.style.visibility = 'visible';
         example.style.visibility = 'visible'
     }
-    }
+}
 
+function nextPage(i) {
+    if (i.target.id === 'next' || i.target.id === 'goButton') { //Goes to next page if Go or Next are clicked
+        pageNum++;
+    }
+    else if (i.target.id === 'prev') { //Goes to first Page when Back is clicked
+        pageNum = 0;
+    }
     changePage()
+}
 
-    function nextPage(i) {
-        if (i.target.id === 'next' || i.target.id === 'goButton') {
-            pageNum++;
-        }
-        else if (i.target.id === 'prev') {
-            pageNum = 0;
-        }
-        changePage()
-    }
-    
+//All button event listeners
 next.addEventListener('click', nextPage)
 goButton.addEventListener('click', nextPage)
 prev.addEventListener('click', nextPage)
